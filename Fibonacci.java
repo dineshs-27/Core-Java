a33b7ffb85ee191de0565e4f04ae8ac787b860b2
Dinesh S <111220091+dineshs-27@users.noreply.github.com>
2022-09-24 10:00:46 +0530

    Add files via upload

---
+++ Fibonacci.java
@@ -0,0 +1,39 @@
+package Learn;
+
+import java.util.Scanner;
+
+public class Fibonacci {
+    public static void main(String[] args) {
+        Fibonacci obj = new Fibonacci();
+        obj.Result();
+        obj.palindrome();
+    }
+
+    public void Result() {
+        Scanner getInput = new Scanner(System.in);
+        System.out.println("Enter a Number : ");
+        int a = getInput.nextInt();
+        int R = 1;
+        for (int i = 1; i <= a; i++) {
+            R = R * i;
+        }
+        System.out.println("Factorial : " + R);
+    }
+
+    public void palindrome() {
+        Scanner getInput = new Scanner(System.in);
+        System.out.println("Enter the String : ");
+        String S1 = getInput.nextLine();
+        String S2 = "";
+        for( int i = S1.length()-1; i >= 0; i--) {
+            char C = S1.charAt(i);
+            String R = Character.toString(C);
+            S2 = S2 + R;
+        }
+        if (S1.equals(S2)) {
+            System.out.println("Its is a Palindrome Word");
+        } else {
+            System.out.println("Its is Not a Palindrome Word");
+        }
+    }
+}
