1ffe7d8468d3e527a131f239ffc7ed3956821149
Dinesh S <111220091+dineshs-27@users.noreply.github.com>
2022-11-26 17:46:42 +0530

    Update Swap.java

--- Swap.java
+++ Swap.java
@@ -1,14 +1,19 @@
 package Learn;
 
+import java.util.Scanner;
+
 public class Swap {
     public static void main(String[] args){
+        Scanner getInput = new Scanner(System.in);
         int a,b;
-        a = 27;
-        b = 3;
-        System.out.println("Before Swapping : "+a+" "+b);
+        System.out.println("Enter the Number a : ");
+        a = getInput.nextInt();
+        System.out.println("Enter the Number b : ");
+        b = getInput.nextInt();
+        System.out.println("Before Swapping a & b : "+a+" "+b);
         a = a+b;
         b = a-b;
         a = a-b;
-        System.out.println("After Swapping : "+a+" "+b);
+        System.out.println("After Swapping a & b : "+a+" "+b);
     }
 }
