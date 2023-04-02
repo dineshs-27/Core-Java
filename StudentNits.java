dacb8be24c65f53ce8ea09c09773b8cc82a992e2
Dinesh S <111220091+dineshs-27@users.noreply.github.com>
2023-04-02 10:12:11 +0530

    Add files via upload

---
+++ StudentNits.java
@@ -0,0 +1,14 @@
+package Learn;
+
+public class StudentNits {
+    public static void main(String[] args){
+        StudentNits obj = new StudentNits();
+        obj.info();
+    }
+
+    public void info(){
+        String S = "John";
+        int R = 25;
+        System.out.println("Name : " + S + "\n" + "Roll no : " + R);
+    }
+}
