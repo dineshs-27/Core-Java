f1d2bbaac6ea0c7ad366715f173032d1c68b35d4
Dinesh S <111220091+dineshs-27@users.noreply.github.com>
2024-07-06 17:24:42 +0530

    Add files via upload

---
+++ Mul_2float.java
@@ -0,0 +1,10 @@
+package Java_Basic_Programs;
+
+public class Mul_2float {
+    public static void main(String[] args){
+        float a = 1.5F;
+        float b = 2.0F;
+        float p = a*b;
+        System.out.println("Product : " + p);
+    }
+}
